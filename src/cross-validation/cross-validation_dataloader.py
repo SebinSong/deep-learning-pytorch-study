a50945b4d79f1d077677407d5211ef769745b60d
SebinSong <songxevin@gmail.com>
2026-01-12 22:04:11 +1300

    train model with mini batches

--- src/cross-validation/cross-validation_dataloader.py
+++ src/cross-validation/cross-validation_dataloader.py
@@ -6,6 +6,8 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, TensorDataset
 
+unique_count = lambda t: torch.unique(t).numel()
+
 iris = sns.load_dataset('iris')
 
 data = torch.tensor( iris[iris.columns[:-1]].values ).float()
@@ -14,22 +16,82 @@ labels = torch.zeros(len(data), dtype=torch.long)
 labels[iris['species'] == 'versicolor'] = 1
 labels[iris['species'] == 'virginica'] = 2
 
-fakedata = np.tile( np.array([1, 2, 3, 4]), (10, 1) ) + np.tile( 10 * np.arange(1, 11), (4, 1)).T
-fakelabels = (np.arange(10) > 4).astype(int)
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2)
 
-train_data, test_data, train_labels, test_labels = train_test_split(fakedata, fakelabels, test_size=0.2)
+train_dataset = TensorDataset(train_data, train_labels)
+test_dataset = TensorDataset(test_data, test_labels)
 
-# convert these into pytorch datasets
-train_dataset = TensorDataset(torch.tensor(train_data), torch.tensor(train_labels))
-test_dataset = TensorDataset(torch.tensor(test_data), torch.tensor(test_labels))
+# Setting shuffle=True has the data reshuffled at every epoch.
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=12)
+test_loader = DataLoader(test_dataset, batch_size=test_dataset.tensors[0].shape[0]) # shuffle option of DataLoader defaults to False.
 
-train_loader = DataLoader(train_dataset, batch_size=3)
-test_loader = DataLoader(test_dataset)
+def createNewModel(n_input, n_hidden_unit, n_output):
+  ANNiris = nn.Sequential(
+    nn.Linear(n_input, n_hidden_unit), # input -> hidden0
+    nn.ReLU(),
+    nn.Linear(n_hidden_unit, n_hidden_unit), # hidden0 -> hidden1
+    nn.ReLU(),
+    nn.Linear(n_hidden_unit, n_output) # hidden1 -> output
+  )
 
-print('TRAIN DATA:')
-for batch, label in train_loader:
-  print(f'{batch=}, {label=}')
-# dataset = TensorDataset(torch.tensor(fakedata), torch.tensor(fakelabels))
-# fakedataLdr = DataLoader(dataset, shuffle=True)
+  # loss function and an optimizer
+  loss_func = nn.CrossEntropyLoss()
+  optimizer = torch.optim.SGD(ANNiris.parameters(), lr=0.01)
 
-# TODO: split the real-data using DataLoader!
+  return ANNiris, loss_func, optimizer
+
+num_epochs = 500
+
+def trainTheModel (model, loss_func, optimizer):
+  # init accuracy arrays
+  train_acc = np.zeros(num_epochs)
+  test_acc = np.zeros(num_epochs)
+
+  # loop over epochs
+  for epoch_i in range(num_epochs):
+    batch_acc = np.zeros(len(train_loader))
+    
+    for i, pair in enumerate(train_loader):
+      X, y = pair
+
+      # forward prop
+      y_hat = model(X)
+      loss = loss_func(y_hat, y)
+
+      # backward prop
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+      pred_labels = torch.argmax(y_hat, axis=1)
+      batch_acc[i] = 100 * torch.mean( (pred_labels == y).float() )
+    
+    train_acc[epoch_i] = np.mean(batch_acc)
+
+    # compute the test accuracy
+    # NOTE: test_loader is an iterable. to use it with next() built-in python function, it must be
+    #       converted to an iterator first (which is done by usinng iter() built-in function)
+    X, y = next(iter(test_loader))
+    pred_labels = torch.argmax(model(X), axis=1)
+    test_acc[epoch_i] = 100 * torch.mean( (pred_labels == y).float() ).item()
+
+  # function output
+  return train_acc, test_acc
+
+net, loss_func, optimizer = createNewModel(
+  n_input=data.shape[1],
+  n_hidden_unit=64,
+  n_output=unique_count(labels)
+)
+
+train_acc, test_acc = trainTheModel(net, loss_func, optimizer)
+
+fig = plt.figure(figsize=(10, 5))
+
+plt.plot(train_acc, 'ro-')
+plt.plot(test_acc, 'bs-')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy %')
+plt.legend(['Train', 'Test'])
+
+plt.show()
--- src/cross-validation/torch_numpy_pandas_playground.py
+++ src/cross-validation/torch_numpy_pandas_playground.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 import pandas as pd
-
+from torch.utils.data import TensorDataset, DataLoader
 
 # rng = np.random.default_rng()
 # n1 = rng.choice(100, (4, 3), replace=False)
@@ -17,6 +17,25 @@ import pandas as pd
 # n1 = np.tile(np.arange(1, 11) * 10, (4, 1)).T
 # print(n1)
 
-fruits = ["apple", "banana", "cherry"]
-green, yellow, *rest = fruits
-print(f'{green=}, {yellow=}, {rest=}')
+# fruits = ["apple", "banana", "cherry"]
+# green, yellow, *rest = fruits
+# print(f'{green=}, {yellow=}, {rest=}')
+
+# data = (torch.randn(100, 3) * 100).int()
+# labels = torch.randint(0, 2, (100,))
+# dataset = TensorDataset(data, labels)
+
+# batch_size = 5
+# dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+# num_epochs = 10
+
+# for epoch_i in range(num_epochs):
+#   for i, pair in enumerate(dataloader):
+#     X, y = pair
+#     if i == 0:
+#       print(f'epoch[{epoch_i}] - X: {X}')
+
+t1 = torch.randint(1, 15, (4, 5))
+numel = t1.numel()
+unique_t1 = torch.unique(t1)
+print(unique_t1, unique_t1.numel())
