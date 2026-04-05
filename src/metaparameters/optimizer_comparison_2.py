873d398992f980b2171a837607c3513d951245b9
SebinSong <songxevin@gmail.com>
2026-04-05 13:37:26 +1200

    comparing optimizer performances with varing learning rates

--- src/metaparameters/optimizer_comparison_2.py
+++ src/metaparameters/optimizer_comparison_2.py
@@ -35,37 +35,6 @@ labels = torch.hstack((
 
 dataset = TensorDataset(data, labels)
 
-draw_data_plot = False
-if draw_data_plot:
-  fig = plt.figure(figsize=(5,5))
-  plt.plot(
-    data[labels == 0, 0],
-    data[labels == 0, 1],
-    'bs',
-    alpha=.5,
-    label='Cluster A'
-  )
-  plt.plot(
-    data[labels == 1, 0],
-    data[labels == 1, 1],
-    'ko',
-    alpha=.5,
-    label='Cluster B'
-  )
-  plt.plot(
-    data[labels == 2, 0],
-    data[labels == 2, 1],
-    'r^',
-    alpha=.5,
-    label='Cluster C'
-  )
-
-  plt.legend()
-  plt.xlabel('Dim 1')
-  plt.ylabel('Dim 2')
-  plt.title('Qwerties!')
-  plt.show()
-
 def split_data (dset, train_prop=.8, batch_size=32):
   sample_size = len(dset)
   train_size = int(sample_size * train_prop)
@@ -153,94 +122,30 @@ def train_model(train_ldr, test_ldr, optim_name = 'SGD', learning_rate=0.01):
     model
   )
 
-def plot_results(
-  train_acc,
-  test_acc,
-  losses,
-  model,
-  optim_name = 'SGD'
-):
-  global data
-
-  with torch.no_grad():
-    y_hat = model(data)
-
-  predictions = torch.argmax(y_hat, dim=1)
-  total_accuracy = (predictions == labels).float().mean().item() * 100
-
-  acc_by_group = np.zeros(3)
-
-  for i in range(3):
-    preds_per_group = predictions[labels == i] # pred results for class i
-    acc_by_group[i] = (preds_per_group == i).float().mean() * 100
-
-  # Create the figure
-  fig, ax = plt.subplots(2, 2, figsize=(10, 6))
-
-  # plot for losses
-  ax[0, 0].plot(losses)
-  ax[0, 0].set_ylabel('Loss')
-  ax[0, 0].set_xlabel('Epoch')
-  ax[0, 0].set_title(f'{optim_name}: Losses')
-
-  # plot the  accuracies
-  ax[0, 1].plot(train_acc, label='Train acc')
-  ax[0, 1].plot(test_acc, label='Test acc')
-  ax[0, 1].set_ylabel('Accuracy (%)')
-  ax[0, 1].set_xlabel('Epoch')
-  ax[0, 1].legend()
-  ax[0, 1].set_title(f'{optim_name}: Accuracies')
-
-  # bar plots for total accuracy by groups
-  ax[1, 0].bar(range(3), acc_by_group)
-  ax[1, 0].set_ylim([
-    np.min(acc_by_group) - 5,
-    np.max(acc_by_group) + 5
-  ])
-  ax[1, 0].set_xticks([0, 1, 2])
-  ax[1, 0].set_xlabel('Group')
-  ax[1, 0].set_ylabel('Accuracy (%)')
-  ax[1, 0].set_title(f'{optim_name}: Accuracy by group (total acc: {total_accuracy:.2f}%)')
-
-  # scatter plot of correct / incorrect labeled data
-  idx_mask_incorrect = predictions != labels
-
-  dot_styles = ['bs', 'ko', 'g^']
-  class_name = ['Cluster A', 'Cluster B', 'Cluster C']
-  ax4 = ax[1, 1]
-
-  # Plot all dots first
-  for i in range(3):
-    ax4.plot(
-      data[labels == i, 0],
-      data[labels == i, 1],
-      dot_styles[i],
-      label=class_name[i],
-      alpha=.5
-    )
-
-  # Draw marks for incorrect predictions
-  ax4.plot(
-    data[idx_mask_incorrect, 0],
-    data[idx_mask_incorrect, 1],
-    'rx',
-    label='Incorrect'
-  )
-  ax4.legend()
-
-  plt.tight_layout()
-  plt.show(block=False)
-
+learing_rates = np.logspace(np.log10(1e-5), np.log10(0.1), 20)
 optimizer_types = ['Adam', 'RMSprop', 'SGD']
 
+final_performances = np.zeros((len(learing_rates), len(optimizer_types)))
+
 train_loader, test_loader = split_data(dataset)
-for oi, optim_type in enumerate(optimizer_types):
-  all_train_acc, all_test_acc, all_losses, model =\
-    train_model(train_loader, test_loader, optim_type)
 
-  plot_results(
-    all_train_acc, all_test_acc, all_losses, model,
-    optim_name=optim_type
-  )
+for idx_o, opt_type in enumerate(optimizer_types):
+  for idx_l, lr in enumerate(learing_rates):
+    all_train_acc, all_test_acc, all_losses, model =\
+      train_model(train_loader, test_loader, opt_type, lr)
 
+    final_performances[idx_l, idx_o] = np.mean( all_test_acc[-10:] )
+
+# plot the results
+plt.plot(
+  learing_rates,
+  final_performances,
+  'o-',
+  linewidth=2
+)
+plt.legend(optimizer_types)
+plt.xscale('log')
+plt.xlabel('Learing rates')
+plt.ylabel('Test accuracy (ave. last 10 epochs)')
+plt.title('Comparison of optimizers by learning rates')
 plt.show()
--- src/metaparameters/seaborn_practice.py
+++ src/metaparameters/seaborn_practice.py
@@ -3,22 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ucimlrepo import fetch_ucirepo
 
-sns.set_theme()
-tips = sns.load_dataset('tips')
-penguins = sns.load_dataset('penguins')
-fmri = sns.load_dataset("fmri")
+# sns.set_theme()
+# tips = sns.load_dataset('tips')
+# penguins = sns.load_dataset('penguins')
+# fmri = sns.load_dataset("fmri")
 
-def kde_with_histogram():
-  wine_df = fetch_ucirepo(id=186) # wine-quality dataset
-  wine_features = wine_df.data.features
+# def kde_with_histogram():
+#   wine_df = fetch_ucirepo(id=186) # wine-quality dataset
+#   wine_features = wine_df.data.features
 
-  wine_features = (wine_features - wine_features.mean()) / wine_features.std()
+#   wine_features = (wine_features - wine_features.mean()) / wine_features.std()
 
-  sns.histplot(wine_features, kde=True, color='skyblue', stat='density')
-  plt.title('Histogram + KDE (Density) for Wine Alcohol Content')
-  plt.xlabel('Alcohol Percentage')
-  plt.ylabel('Density')
-  plt.show()
+#   sns.histplot(wine_features, kde=True, color='skyblue', stat='density')
+#   plt.title('Histogram + KDE (Density) for Wine Alcohol Content')
+#   plt.xlabel('Alcohol Percentage')
+#   plt.ylabel('Density')
+#   plt.show()
 
-print(fmri)
+# print(fmri)
 # kde_with_histogram()
+
+lrs = np.logspace(np.log(1e-5), np.log10(0.1), 20)
+lrs2 = np.logspace(0, 10, 11, base=2).astype(int)
+print(lrs2)
