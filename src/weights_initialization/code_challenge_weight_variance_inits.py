a1cdb5106eb0fe825442dfb4d7f509dee3a14db8
SebinSong <songxevin@gmail.com>
2026-05-28 21:15:42 +1200

    appropreate variance matters when initializing weights with normal distribution

--- src/weights_initialization/code_challenge_weight_variance_inits.py
+++ src/weights_initialization/code_challenge_weight_variance_inits.py
@@ -26,6 +26,7 @@ dataset = TensorDataset(data_t, labels_t)
 D_in = data_t.shape[1]
 D_out = torch.unique(labels_t).numel()
 num_epochs = 10
+n_hist_bins = 80
 
 def get_data_labels_from_ldr(ldr):
   tensors = ldr.dataset.dataset.tensors
@@ -94,5 +95,86 @@ def train_model(train_ldr, test_ldr, std=1):
 
     all_batch_acc = []
     all_batch_losses = []
-    for batch_x, batch_y in train_ldr
-      pass
+
+    for batch_x, batch_y in train_ldr:
+      # forward-pass and compute the loss
+      y_hat = model(batch_x)
+      loss = loss_func(y_hat, batch_y)
+      all_batch_losses.append( loss.item() )
+
+      # backprop
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      
+      # compute the batch_acc
+      batch_acc = (torch.argmax(y_hat, dim=1) == batch_y).float().mean() * 100
+      all_batch_acc.append( batch_acc.item() )
+
+    ave_loss = np.mean( all_batch_losses )
+    ave_train_acc = np.mean( all_batch_acc )
+    all_losses[epoch_i] = ave_loss
+    all_train_acc[epoch_i] = ave_train_acc
+
+    print(f'[Epoch {epoch_i}] loss={ave_loss:.3f}, train_accuracy={ave_train_acc:.3f}')
+
+    # compute the test_accuracy
+    model.eval()
+    with torch.no_grad():
+      test_predictions = torch.argmax(model(test_x), dim=1)
+    
+    test_acc = (test_predictions == test_y).float().mean() * 100
+    all_test_acc[epoch_i] = test_acc.item()
+
+  return all_train_acc, all_test_acc, all_losses, model
+
+def get_param_histo_from_model(model):
+  all_params = np.array([])
+  for name, p in model.named_parameters():
+    if 'weight' in name:
+      all_params = np.concatenate((all_params, p.data.detach().numpy().flatten()), axis=0)
+
+  counts, edges = np.histogram(all_params, bins=n_hist_bins)
+  center_vals = (edges[:-1] + edges[1:]) / 2
+
+  return center_vals, counts
+
+std_dev_list = np.logspace(np.log10(1e-4), np.log10(10), 18, base=10)
+acc_results = np.zeros(len(std_dev_list)) # capturing the test accuracy for each std
+histo_data = np.zeros((len(std_dev_list), 2, n_hist_bins))
+
+train_loader, test_loader = split_data(dataset, batch_size=64)
+
+for std_idx, std in enumerate(std_dev_list):
+  time_start = time.perf_counter()
+  train_acc, test_acc, losses, model = train_model(train_loader, test_loader, std=std)
+
+  model_acc =  np.mean(test_acc[-3:])
+  acc_results[std_idx] = model_acc
+  histo_x, histo_counts = get_param_histo_from_model(model)
+  histo_data[std_idx, 0, :] = histo_x
+  histo_data[std_idx, 1, :] = histo_counts
+
+  elapsed_time_sec = time.perf_counter() - time_start
+  print(f'[{std_idx + 1}/{len(std_dev_list)} iteration] Elapsed: {elapsed_time_sec:.2f} sec, final accuracy: {model_acc:.2f}%')
+
+# plot the results
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 5))
+
+ax1.plot(std_dev_list, acc_results, 's-')
+ax1.set_xlabel('Std for weight init')
+ax1.set_xscale('log')
+ax1.set_ylabel('Test accuracy (final-3, %)')
+ax1.set_title('Test accuracy per std')
+
+len_std_dev = len(std_dev_list)
+for std_idx, std in enumerate(std_dev_list):
+  hist_x = histo_data[std_idx, 0, :]
+  hist_y = histo_data[std_idx, 1, :]
+  color_f = 1 - (std_idx / len_std_dev)
+  ax2.plot(hist_x, hist_y, label=f'{std:.6f}', color=[color_f, 0.4, color_f])
+ax2.set_xlabel('Weight value')
+ax2.set_ylabel('Count')
+ax2.legend(bbox_to_anchor=(1, 1), loc='upper left')
+
+plt.show()
--- src/weights_initialization/torch_numpy_pandas_playground.py
+++ src/weights_initialization/torch_numpy_pandas_playground.py
@@ -1,6 +1,22 @@
 import numpy as np
 
-data = [1, 2, 1, 5, 6, 6]
-counts, edges = np.histogram(data, 3)
-print(counts)
-print(edges)
+if False:
+  data = [1, 2, 1, 5, 6, 6]
+  counts, edges = np.histogram(data, 3)
+  middles = (edges[:-1] + edges[1:]) / 2
+  print(counts)
+  print(edges)
+  print(middles)
+
+if True:
+  # np.logspace() research
+  range1 = np.logspace(np.log10(1), np.log10(10**5), 6, base=10)
+  # np.logspace(a, b, (b-a)+1, base=c):
+  # - This pretty much means I want to create an ndarray that ranges from c**a to c**b in log-c space.
+  range2 = np.logspace(1, 5, 5, base=2)
+
+  print(range2)
+  nd_arr1 = 2**np.arange(0, 10)
+  nd_arr2 = np.arange(0, 10)**2
+  print(nd_arr1)
+  print(nd_arr2)
